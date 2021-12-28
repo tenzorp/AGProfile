d97422781e9cbe1141704f1c898cf4be5b74f37c
Tenzin Dorjee <tenzorp@protonmail.com>
2021-12-27 18:00:18 -0800

    main page finished

--- App.js
+++ App.js
@@ -19,7 +19,7 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{}}>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
         <Stack.Screen
           name="Profile"
           component={HomeScreen}
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View, Image, StatusBar } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const profPic = require("../assets/profPic.jpg");
 
@@ -14,14 +16,75 @@ export default class HomeScreen extends Component {
         <View style={styles.imageContainer}>
           <Image style={styles.image} source={profPic} />
         </View>
-
-        <View style={styles.infoContainer}>
-          <Text>Name</Text>
-          <Text>Phone</Text>
-          <Text>Email</Text>
-          <Text>Tell us about yourself</Text>
+        <View
+          style={{
+            backgroundColor: "#FFF",
+            width: 50,
+            height: 50,
+            borderRadius: 25,
+            position: "absolute",
+            top: 160,
+            right: 125,
+            alignItems: "center",
+            justifyContent: "center",
+            shadowOffset: {width: 2, height: 2},
+            shadowRadius: 3,
+            shadowOpacity: 0.15
+          }}
+        >
+          <MaterialCommunityIcons name="pencil" size={26} color="#446DDE" />
         </View>
-        <StatusBar barStyle="dark-content" />
+
+        <View style={styles.lowerContainer}>
+          {infoComponent("Name", "Micah Smith")}
+          {infoComponent("Phone", "(208) 206-5039")}
+          {infoComponent("Email", "micahsmith@gmail.com")}
+          {infoComponent(
+            "Tell us about yourself",
+            "Hi my name is Mica Smith. I am from Mesa but go to school in Salt Lake City. \nI make this drive all the time and have plenty"
+          )}
+        </View>
+        <StatusBar barStyle="dark-content"/>
+      </View>
+    );
+  }
+}
+
+function infoComponent(title, info) {
+  if (title == "Tell us about yourself") {
+    return (
+      <View style={styles.infoContainer2}>
+        <View style={{ flexShrink: 1 }}>
+          <Text style={styles.infoHeader}>{title}</Text>
+          <Text numberOfLines={3} style={styles.infoText}>
+            {info}
+          </Text>
+        </View>
+        <View style={{ alignSelf: "center" }}>
+          <AntDesign
+            name="right"
+            size={24}
+            color="#B3B3B3"
+            style={{ padding: 10, flexBasis: 50 }}
+          />
+        </View>
+      </View>
+    );
+  } else {
+    return (
+      <View style={styles.infoContainer}>
+        <View style={{ flexShrink: 1 }}>
+          <Text style={styles.infoHeader}>{title}</Text>
+          <Text style={styles.infoText}>{info}</Text>
+        </View>
+        <View style={{ alignSelf: "center" }}>
+          <AntDesign
+            name="right"
+            size={24}
+            color="#B3B3B3"
+            style={{ padding: 10, flexBasis: 50 }}
+          />
+        </View>
       </View>
     );
   }
@@ -32,12 +95,12 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#FFF",
     justifyContent: "center",
-    paddingHorizontal: 50,
+    paddingHorizontal: 40,
+    paddingTop: 100
   },
   titleContainer: {
     flex: 0.25,
     justifyContent: "center",
-    backgroundColor: "aliceblue",
   },
   header: {
     textAlign: "center",
@@ -47,16 +110,46 @@ const styles = StyleSheet.create({
   },
   imageContainer: {
     flex: 1,
-    backgroundColor: "purple",
+    alignSelf: "center",
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+    overflow: "hidden",
+    borderColor: "#446DDE",
+    borderWidth: 6,
+    margin: 10,
   },
   image: {
     flex: 1,
     alignSelf: "center",
     resizeMode: "contain",
   },
-  infoContainer: {
+  lowerContainer: {
     flex: 3,
-    backgroundColor: "green",
     textAlign: "left",
+  },
+  infoContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    borderBottomColor: "#E5E5E5",
+    borderBottomWidth: 1,
+    height: 60,
+  },
+  infoContainer2: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    borderBottomColor: "#E5E5E5",
+    borderBottomWidth: 1,
+    height: 100,
+  },
+  infoHeader: {
+    fontSize: 14,
+    color: "#B3B3B3",
+    fontWeight: "bold",
+    marginVertical: 7,
+  },
+  infoText: {
+    fontSize: 14,
+    fontWeight: "bold",
   },
 });
